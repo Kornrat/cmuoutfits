6b542528a7f70a5d8de8dfece7f84c2c577d12d5
Kornrat <keuchuka@andrew.cmu.edu>
2019-12-03 22:25:02 -0500

    new

--- js.js
+++ js.js
@@ -1,8 +1,57 @@
-function changeAnswer() {
 
-					  $(".answer").attr("class", "animated fadeIn");
+$('.scrollTo').click(function(){
+	var offset = 60;
+	$('html, body').animate({
+    scrollTop: ($('#target-element').offset().top - offset)
+},500);
+});
+
+
+function changeAnswerCFA(){
+
+
+	$("#rightCFA").removeAttr('id');
+	$('#rightCFA').addClass('animated fadeIn');
+
 
 		}
 
+
+function changewrongAnswerCFA() {
+
+	$("#wrongCFA").removeAttr('id');
+	$('#wrongCFA').addClass('animated fadeIn');
+		}
+
+
+function changeAnswerCIT() {
+
+	$("#rightCIT").removeAttr('id');
+	$('#rightCIT').addClass('animated fadeIn');
+
+		}
+
+
+function changewrongAnswerCIT() {
+
+	$("#wrongCIT").removeAttr('id');
+	$('#wrongCIT').addClass('animated fadeIn');
+
+		}
+
+function changeAnswerDIET() {
+
+	$("#rightDIET").removeAttr('id');
+	$('#rightDIET').addClass('animated fadeIn');
+
+		}
+
+
+function changewrongAnswerDIET() {
+
+	$("#wrongDIET").removeAttr('id');
+	$('#wrongDIET').addClass('animated fadeIn');
+
+		}
 
 
